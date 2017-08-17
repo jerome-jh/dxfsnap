50868de447c6bc6fe69bd5d49370cb5b428125b0
Jerome Hourioux <jerome.hourioux@parrot.com>
2017-08-17 16:21:20 +0200

    Can round to 'any' power of ten

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Thanks to the structure of DXF files, the script is also very simple. Maybe too
 simple? Tell me if you encounter problems.
 
 Does not work with binary DXF.
+Does not handle entities defined by an angle.
--- dxfsnap.py
+++ dxfsnap.py
@@ -22,13 +22,52 @@ import itertools
 groups = (range(10,19), range(20,29), range(30,38))
 
 def usage():
-    print(sys.argv[0],'<significant digits> <DXF input file>')
+    print(sys.argv[0],'<power of ten> <DXF input file>')
     print()
-    print('\tRound all X,Y,Z coordinates to the given number of significant digits. Output modified DXF to stdout.')
+    print('\tRound all X,Y,Z coordinates to the given power of 10. Output modified DXF to stdout.')
+    print()
+    print('\tExamples:')
+    print('\t\t' + sys.argv[0], 0, 'toto.dxf > tutu.dxf')
+    print('\t\t- round to nearest integer, output to "tutu.dxf"')
+    print()
+    print('\t\t' + sys.argv[0], -3, 'toto.dxf')
+    print('\t\t- three significant digits after decimal point, output to stdout')
+    print()
+    print('\t\t' + sys.argv[0], 1, 'toto.dxf')
+    print('\t\t- round to multiples of 10, output to stdout')
     print()
 
+def get_round_f(pot):
+    if pot <= 0:
+        ## Use built-in Python round()-ing
+        ## round(0.5) = 0 but that does not look too bad
+        def round_f(f):
+            return round(f, -pot)
+        return round_f
+    else:
+        ## Round "as I learned in school" in this case because
+        ## pot=1, round(5)=0 will never look acceptable
+        m = 10**pot
+        def round_f(f):
+            if f < 0:
+                sign = -1
+            else:
+                sign = 1
+            return m * int(sign * 0.5 + f / m)
+        return round_f
+
+def test_round():
+    assert(get_round_f(1)(5) == 10)
+    assert(get_round_f(1)(4.9999) == 0)
+    assert(get_round_f(1)(-5) == -10)
+    assert(get_round_f(1)(-4.9999) == 0)
+    assert(get_round_f(2)(150) == 200)
+    assert(get_round_f(2)(149.9999999) == 100)
+    assert(get_round_f(2)(-150) == -200)
+    assert(get_round_f(2)(-149.9999999) == -100)
+
 if __name__ == '__main__':
-    ## Significant digits
+    test_round()
     try:
         digits = int(sys.argv[1])
         filein = open(sys.argv[2], 'rt')
@@ -37,6 +76,7 @@ if __name__ == '__main__':
         quit()
     fileout = sys.stdout
     cont = True
+    do_round = get_round_f(digits)
     while cont:
         group = filein.readline()
         value = filein.readline()
@@ -44,7 +84,7 @@ if __name__ == '__main__':
             g = int(group)
             if g in itertools.chain(*groups):
                 v = float(value)
-                v = round(v, digits)
+                v = do_round(v)
                 fileout.write(group)
                 fileout.write(str(v) + os.linesep)
             else:
